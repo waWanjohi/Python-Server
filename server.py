f3ff1b3237e9b4fc08fa7e8d70d9c92b735f2ede
Gem <thestickman@corp.org>
2021-05-26 11:20:18 +0300

    Added new file

---
+++ server.py
@@ -0,0 +1,19 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class ServeHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('content-type', 'text/html')
+		self.end_headers()
+		self.wfile.write('Server is working fine!'.encode())
+
+
+
+
+def main():
+	httpserver = HTTPServer(('localhost', 8000), ServeHandler)
+	print('Server running on http://localhost:8000')
+	httpserver.serve_forever()
+
+if __name__ == '__main__':
+	main()
