da223550b3c230078bfe2cb802e90db3671b24f6
Gem <thestickman@corp.org>
2021-05-26 11:31:17 +0300

    Updated server.py

--- server.py
+++ server.py
@@ -1,20 +1,20 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+
 class ServeHandler(BaseHTTPRequestHandler):
-	def do_GET(self):
-		self.send_response(200)
-		self.send_header('content-type', 'text/html')
-		self.end_headers()
-		self.wfile.write('Server is working fine! Path = '.encode())
-		self.wfile.write(self.path.encode())
-
-
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write('Server is working fine! Path = '.encode())
+        self.wfile.write(self.path.encode())
 
 
 def main():
-	httpserver = HTTPServer(('localhost', 8000), ServeHandler)
-	print('Server running on http://localhost:8000')
-	httpserver.serve_forever()
+    httpserver = HTTPServer(('localhost', 8000), ServeHandler)
+    print('Server running on http://localhost:8000')
+    httpserver.serve_forever()
+
 
 if __name__ == '__main__':
-	main()
+    main()
